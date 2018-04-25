48928fb6cf53a5725388dd05ece42fa1e8bb5e6e
april1026 <36957874+april1026@users.noreply.github.com>
2018-04-25 23:53:05 +0800

    Update gistfile1.cpp

--- gistfile1.cpp
+++ gistfile1.cpp
@@ -7,36 +7,41 @@ int main() {
 	cin >> num;
 	int price[num];
 	int total = 0;
+	int listnum = 1;
 	for (int i = 0; i < num; i++) {
 		cin >> price[i];
 		total += price[i];
+		listnum *= 2;
 	}
 	
-	int list[num * (num-1) / 2][num];
+	int list[num][listnum];
 	int change = 1;
 	bool save = 0;
-	for (int i = 0; i < (num * (num-1) / 2); i++) {
-		for (int j = 1; j <= num; j++) {
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j < listnum; j++) {
 			if (i == 0)
-				list[j-1][i] = (j % 2); 
+				list[i][j] = j % 2; 
 			else if (j % change == 0){
 				if (save == 0)
 					save = 1;
 				else if (save == 1)
 					save = 0;
-				list[j-1][i] = save;
-			}
+				list[i][j] = save;
+			}else
+				list[i][j] = save;
 		}
 		change *= 2;
+		save = 1;
 	}
 	
-	for (int i = 0; i < num * (num-1); i++) {
-		for (int j = 0; j < num; j++) {
+	/*
+	for (int i = 0; i < num; i++) {
+		for (int j = 0; j < listnum; j++) {
 		cout << list[i][j];
 		}
 		cout << endl;
 	}
-	
+	*/
 	
  return 0; 
 }
